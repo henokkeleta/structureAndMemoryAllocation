9d34a1ac2140bf2678419382fed88c3538f6d9df
Henok Berhe <henokberhe@henoks-macbook-pro.local>
2018-12-23 12:42:58 -0500

    Initial Commit

---
+++ cpra/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cpra
+//
+//  Created by Henok Berhe on 12/23/18.
+//  Copyright © 2018 Henok Berhe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
